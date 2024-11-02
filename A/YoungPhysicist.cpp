9c7028e6e781c8beee18efba72a1880b73f8ed24
Michael <mhe@consumertrack.com>
2024-11-01 22:49:50 -0400

    Completed 69A

---
+++ A/YoungPhysicist.cpp
@@ -0,0 +1,49 @@
+// https://codeforces.com/problemset/problem/69/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void solve() {
+	int n;
+	cin >> n;
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	while(n--) {
+		int x1;
+		int y1;
+		int z1;
+		cin >> x1 >> y1 >> z1;
+		x += x1;
+		y += y1;
+		z += z1;
+	}
+	if(x == 0 && y == 0 && z == 0) {
+		cout << "YES";
+	} else {
+		cout << "NO";
+	}
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
